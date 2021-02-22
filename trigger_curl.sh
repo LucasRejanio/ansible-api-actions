e6b702965f6620ba22a1c37e482f5e7e7f203117
LucasRejanio <lucas.rejanio@gmail.com>
2021-02-22 17:52:31 -0300

    Add new file of .sh

--- trigger_curl.sh
+++ trigger_curl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Adicione o seu TOKEN:
+# Adicione o seu TOKEN de desenvolvedor:
 curl -u "TOKEN:" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/LucasRejanio/test-api-actions/actions/workflows/test-api.yml/dispatches \
+  https://api.github.com/repos/LucasRejanio/ansible-api-actions/actions/workflows/test-api.yml/dispatches \
   -d '{"ref":"main"}'
