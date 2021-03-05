c8798b7f5cbbb86aae3a10e8f7f3a31019c97a27
Lucas Rejanio <52427398+LucasRejanio@users.noreply.github.com>
2021-03-05 11:39:00 -0300

    Update trigger_curl inputs.sh

--- trigger_curl inputs.sh
+++ trigger_curl inputs.sh
@@ -4,12 +4,12 @@
 curl -u "TOKEN:" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/LucasRejanio/ansible-api-actions/actions/workflows/test-dispatch-inputs.yml/dispatches \
+  https://api.github.com/repos/LucasRejanio/ansible-api-actions/actions/workflows/test-api-inputs.yml/dispatches \
   -d '{"ref":"main", "inputs": {"branch_name": "lr-test-api"}}'
 
 ## Passando uma variavel
 curl -u "TOKEN:" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/LucasRejanio/ansible-api-actions/actions/workflows/test-dispatch-inputs.yml/dispatches \
+  https://api.github.com/repos/LucasRejanio/ansible-api-actions/actions/workflows/test-api-inputs.yml/dispatches \
   -d '{"ref":"main", "inputs": {"branch_name": "'"$branch_name"'"}}'
